0095713c0df1b42184b5dfc4ece5d02f76150a27
Benjamin Sweett <bensweett@dhcp-134-46.cu-wireless-students.carleton.ca>
2014-12-02 16:48:47 -0500

    Added Dialog library and updated build to Java 8u25. Added functionality for reading a user selected directory based on the dialogs input and creating the map of song objects.

--- src/com/cstream/CApplicationController.java
+++ src/com/cstream/CApplicationController.java
@@ -1,9 +1,13 @@
 package com.cstream;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 import javafx.event.Event;
 import javafx.scene.Parent;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
@@ -11,7 +15,12 @@ import com.cstream.controller.Controller;
 import com.cstream.media.MediaBarController;
 import com.cstream.media.MediaInfoController;
 import com.cstream.media.MediaLibraryController;
+import com.cstream.model.Song;
 import com.cstream.tracker.TrackerClient;
+import com.cstream.tracker.TrackerPeer;
+import com.cstream.utils.LibraryUtils;
+import com.cstream.utils.OSUtils;
+import com.sun.org.apache.bcel.internal.generic.ISUB;
 
 public class CApplicationController extends Controller {
 
@@ -33,8 +42,10 @@ public class CApplicationController extends Controller {
 	// TODO: MediaPlayer needs a source of Media to be initialized
 	private MediaPlayer mp;
 	
-	// TODO: Networking Instance
-	private TrackerClient tracker;
+	// TODO: RTP Instance
+	
+	//This Peer
+	private TrackerPeer peer;
 	
 	public void initialize(Stage stage) {
 		this.stage = stage;
@@ -54,8 +65,12 @@ public class CApplicationController extends Controller {
 		addEventHandlers();
 		addEventListeners();
 		
+		//TODO: Check that library was returned correctly and isnt empty
+		// Log warning or display info to user?
+		Map<String, Song> library = buildLibrary();
 		
-		
+		//TODO: Get port? and build peer object for this client
+		//peer = new TrackerPeer(port, library);
 	}
 	
 	public void stop() {
@@ -99,6 +114,49 @@ public class CApplicationController extends Controller {
 		
 		//PropertyChangeDispatcher.getInstance().addListener(GameClient.class, "connected", this, "onConnectionChange");
 		
+	}
+	
+	private Map<String, Song> buildLibrary() {
+		
+		Map<String, Song> library = new HashMap<String, Song>();
+		String path = "";
+		String folder = showPathDialog();
+		String slash = "/";
+		
+		if (OSUtils.isWindows()) {
+			slash = "\\";
+		}
+		
+		if(folder != null) {
+			path = System.getProperty("user.home") + slash + folder;
+		}
+		
+		if(path != null) {
+			library = LibraryUtils.buildLocalLibrary(path);
+		}
+		
+		return library;
+	}
+	
+	
+	protected String showPathDialog() {
+		
+		String defaultV = "/Music";
+		
+		TextInputDialog dialog = new TextInputDialog(defaultV);
+		dialog.setTitle("Music Library Path");
+		dialog.setHeaderText("To share your music with others enter" +
+		"a path to your music folder. Defaults from : " + System.getProperty("user.home") + defaultV);
+		dialog.setContentText("Path to Music: ");
+
+		// Traditional way to get the response value.
+		Optional<String> result = dialog.showAndWait();
+		
+		if (result.isPresent()){
+		    return result.get();
+		}
+
+		return "";
 	}
 	
 }
