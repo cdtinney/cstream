8b68c8259bb38347157d37ae63220d3234040952
cdtinney <colintinney@cmail.carleton.ca>
2014-12-02 15:20:11 -0500

    add FileUtils for getting files inside a directory

---
+++ src/com/cstream/utils/FileUtils.java
@@ -0,0 +1,66 @@
+package com.cstream.utils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class FileUtils {
+	
+	private static Logger LOGGER = Logger.getLogger(FileUtils.class.getName());
+	
+    public static List<File> listFiles(String directoryName, String[] extensions) {
+    	
+    	List<File> results = new ArrayList<File>();    	
+    	
+        File directory = new File(directoryName);
+        
+        File[] fList = directory.listFiles();
+        
+        if (fList == null) {
+        	LOGGER.warning("Cannot list files of a directory which does not exit - " + directoryName);
+        	return results;
+        }
+        
+        for (File file : fList) {
+        	
+        	if (file.isDirectory()) {
+        		
+        		// Resursively add files within sub-directories
+        		results.addAll(listFiles(file.getAbsolutePath(), extensions));
+        		
+        	} else {
+        		
+        		if (extensions == null) {
+        			results.add(file);
+        			continue;
+        		}
+        		
+        		if (validExtension(file, extensions)) {
+        			results.add(file);
+        		}
+        	}
+        	
+        }
+        
+        return results;
+        
+    }
+    
+    private static boolean validExtension(File file, String[] extensions) {
+    	
+    	boolean valid = false;
+    	for (String extension : extensions) {
+			
+			if (file.getName().endsWith(extension) || file.getName().endsWith("." + extension)) {
+				valid = true;
+				break;
+			}
+			
+		}
+    	
+    	return valid;
+    	
+    }
+
+}
