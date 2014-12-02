38e1b005e3fb5254126d89c962f71d52bc6ebf5b
cdtinney <colintinney@cmail.carleton.ca>
2014-12-02 15:28:27 -0500

    useless javadocs

--- src/com/cstream/utils/EnumUtils.java
+++ src/com/cstream/utils/EnumUtils.java
@@ -1,21 +1,7 @@
 package com.cstream.utils;
 
-/**
- * EnumUtils contains a collection of functions for processing ENUMs
- *
- * @author Colin Tinney
- * @version 1.0
- * @since 2014-09-29
- */
 public class EnumUtils {
 	
-	/**
-	 * Checks the value of the ENUM against a string.
-	 * 
-	 * @param Class<E> e
-	 * @param String id
-	 * @return E
-	 */
 	public static <E extends Enum<E>> E lookup(Class<E> e, String id) {   
 
 		try {          
