07ecb317025f067320c128c86b41602164831546
cdtinney <colintinney@cmail.carleton.ca>
2014-12-10 14:50:27 -0500

    cleanup

--- src/com/cstream/logging/LogLevel.java
+++ src/com/cstream/logging/LogLevel.java
@@ -9,7 +9,7 @@ public class LogLevel extends Level {
 	public static final Level STATUS = new LogLevel("STATUS", Level.INFO.intValue() + 1);
 	public static final Level DEBUG = new LogLevel("DEBUG", Level.FINER.intValue() + 1);
 	
-	public LogLevel(String name, int value) {
+	private LogLevel(String name, int value) {
 		super(name, value);
 	}
 
